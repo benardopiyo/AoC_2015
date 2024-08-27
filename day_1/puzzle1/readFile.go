b3e1c4e3fbc52d97576eff2740beab5da23e1d68
beopiyo <benardopiyo13@gmail.com>
2024-08-27 23:28:26 +0300

    feat: add file reader

---
+++ day_1/puzzle1/readFile.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+)
+
+func ReadFile(filePath string) (string, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
