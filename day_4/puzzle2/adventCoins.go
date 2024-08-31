58153995398dd09c2619f4740beb7ec216b5b83f
beopiyo <benardopiyo13@gmail.com>
2024-08-31 19:57:43 +0300

    feat: add function to return Hash with 00000 as prefix

---
+++ day_4/puzzle2/adventCoins.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Finds the lowest number that,
+// when appended to the key, produces a hash starting with five zeroes
+func adventCoinSuffix(key string) int {
+	number := 1
+	for {
+		hashInput := fmt.Sprintf("%s%d", key, number)
+		hash := md5HashToHex(hashInput)
+
+		if strings.HasPrefix(hash, "000000") {
+			return number
+		}
+		number++
+	}
+}
